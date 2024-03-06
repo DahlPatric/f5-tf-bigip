56319a955d8a66fb064b165217f1aa4b2b7cd64d
DahlPatric <120572102+DahlPatric@users.noreply.github.com>
2024-03-06 15:21:13 +0100

    Update main.tf

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -1,13 +1,18 @@
-terraform {
-  cloud {
-    organization = "VolvoCars-F5"workspaces {
-      name = "f5-tf-bigip-noneprod"
-    }
+resource "bigip_fast_http_app" "app2" {
+  application               = "myApp4"
+  tenant                    = "scenario3"
+  virtual_server            {
+    ip                        = "10.1.10.224"
+    port                      = 80
   }
-  required_providers {
-    bigip = {
-      source  = "F5Networks/bigip"
-      version = "1.13.1"
-    }
+  pool_members  {
+    addresses                 = ["10.1.10.120", "10.1.10.121", "10.1.10.122"]
+    port                      = 80
+  }
+  snat_pool_address = ["10.1.10.50", "10.1.10.51", "10.1.10.52"]
+  load_balancing_mode       = "least-connections-member"
+  monitor       {
+    send_string               = "GET / HTTP/1.1\\r\\nHost: example.com\\r\\nConnection: Close\\r\\n\\r\\n"
+    response                  = "200 OK"
   }
 }
